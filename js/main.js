433c6193ed2bc299b00596f00433788d75b1bd70
Kibet <ngeringkwony@gmail.com>
2018-08-01 06:50:45 +0300

    Food description and order done, static

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
 window.sr = ScrollReveal({reset:false, scale: 1.0});
-sr.reveal('.fade-in', {duration:2000,origin:'left', distance: '50px'});
+sr.reveal('.fade-in', {duration:2000, origin:'left', distance: '50px'});
